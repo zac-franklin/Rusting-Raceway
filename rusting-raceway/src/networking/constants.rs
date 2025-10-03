8a3e92fccfe3e66cbb77e0ecb0558c043bbcabf9
vfernandez26 <victored@metaconxept.com>
2025-10-03 09:27:52 -0500

    reset nm-players constants to original value

--- rusting-raceway/src/networking/constants.rs
+++ rusting-raceway/src/networking/constants.rs
@@ -1,7 +1,7 @@
 //! Contains networking constants
 
 /// Session constants
-pub const NUM_PLAYERS: usize = 4;
+pub const NUM_PLAYERS: usize = 2;
 
 /// GGRS constants
 pub const LOCAL_INPUT_DELAY_FRAMES: usize = 2;
